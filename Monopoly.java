1781875ce532be4a778c79f82fc72e84452218b4
YKbabu77 <yepugantikondababu@gmail.com>
2026-07-21 21:01:54 +0530

    Monopoly Problem updated

--- Monopoly.java
+++ Monopoly.java
@@ -10,15 +10,28 @@ public class Monopoly {
             int q = in.nextInt();
             int r = in.nextInt();
             int s = in.nextInt();
-            if((p>q) && (p>r) && (p>s)){
-                System.out.println(((q+r+s)<p)?"YES":"NO");
-            }else if((q>p) && (q>r) && (q>s)){
-                System.out.println(((p+r+s)<q)?"YES":"NO");
-            }else if((r>p) && (r>q) && (r>s)){
-                System.out.println(((q+p+s)<r)?"YES":"NO");
-            }else if((s>p) && (s>q) && (s>r)){
-                System.out.println(((p+q+r)<s)?"YES":"NO");
+            int sum=p+q+r+s;
+            if(sum-p<p){
+                System.out.println("YES");
+            }else if(sum-q<q){
+                 System.out.println("YES");
+            }else if(sum-r<r){
+                 System.out.println("YES");
+            }else if(sum-s<s){
+                 System.out.println("YES");
+            }else{
+                System.out.println("NO");
             }
+            
+            // if((p>q) && (p>r) && (p>s)){
+            //     System.out.println(((q+r+s)<p)?"YES":"NO");
+            // }else if((q>p) && (q>r) && (q>s)){
+            //     System.out.println(((p+r+s)<q)?"YES":"NO");
+            // }else if((r>p) && (r>q) && (r>s)){
+            //     System.out.println(((q+p+s)<r)?"YES":"NO");
+            // }else if((s>p) && (s>q) && (s>r)){
+            //     System.out.println(((p+q+r)<s)?"YES":"NO");
+            // }
         }
     }
 }
